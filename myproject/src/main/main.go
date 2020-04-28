01291c93083873205ff3a50237e7f11bcd316bc5
nakapon <maksminakapon@gmail.com>
2020-04-28 23:36:03 +0900

    change

--- myproject/src/main/main.go
+++ myproject/src/main/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
-	"gosample"
 )
 
 func main() {
-	fmt.Println(gosample.Message) // hello world
+	// fmt.Println(gosample.Message)
+	// fmt.Println(gosample.Message2)
+	param := [...]string{"a", "b"}
+	fmt.Println(param)
+	fmt.Println(sub.param)
 }
