effabb8dacd06592d9578fc3cb94c7492f94dab3
nakapon <maksminakapon@gmail.com>
2020-05-01 00:57:48 +0900

    disp Even and odd

--- myproject/src/main/main.go
+++ myproject/src/main/main.go
@@ -7,6 +7,34 @@ import (
 )
 
 func main() {
-	// aa
-	f.Println(ToLower("hello world"))
+	a := "aaa"
+	b := "bbb"
+	c := "ccc"
+	d := "ddd"
+	e := "eee"
+	f.Println(ToUpper(a), ToUpper(b), ToUpper(c), ToLower(d), ToUpper(e))
+	// f.Println(j)
+
+	// if j == 0 {
+	// 	f.Println("aa")
+	// }
+
+	j := 25
+	for {
+		if j == 0 {
+			f.Println("ゼロです。")
+		}
+		if j%2 == 0 {
+			f.Println("偶数：", j)
+		}
+		if j%2 == 1 {
+			f.Println("奇数：", j)
+		}
+		if j == 0 {
+			break
+		} else {
+			j--
+			continue
+		}
+	}
 }
