e63c8710f7b029f5cacbec7a7bcd849bdef718d9
nakapon <maksminakapon@gmail.com>
2020-05-04 12:50:53 +0900

    Fixes #1,Fixes #2,Fixes #3

--- funcproject/cmd/go.mod
+++ funcproject/cmd/go.mod
@@ -1,3 +1,5 @@
 module funcproject
 
 go 1.13
+
+require github.com/sirupsen/logrus v1.6.0 // indirect
--- funcproject/cmd/main.go
+++ funcproject/cmd/main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"funcproject/calc"
+
+	log "github.com/sirupsen/logrus"
 )
 
+func div(i, j int) (int, error) {
+	if j == 0 {
+		// 自作のエラーを返す
+		return 0, errors.New("divied by zero")
+	}
+	return i / j, nil
+}
+
 func main() {
-	fmt.Println(calc.Sum(3, 8))
+	n, err := div(10, 0)
+	if err != nil {
+		// エラーを出力しプログラムを終了する
+		requestID := 1
+		userIP := "192.168.10.1"
+		requestLogger := log.WithFields(log.Fields{"request_id": requestID, "user_ip": userIP})
+		requestLogger.Info("something happened on that request")
+		requestLogger.Warn("something not great happened")
+	}
+	fmt.Println(n)
 }
